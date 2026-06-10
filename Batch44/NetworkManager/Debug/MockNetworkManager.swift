e7a621cfca57fc3abc4feb6fc44a437d32584660
Shobhakar Tiwari <st.shubh.tiwari@gmail.com>
2026-06-10 10:07:36 -0500

    parse data added

--- Batch44/NetworkManager/Debug/MockNetworkManager.swift
+++ Batch44/NetworkManager/Debug/MockNetworkManager.swift
@@ -12,13 +12,14 @@ final class MockNetworkManager: Sendable, NetworkProtocol {
     
     func getDataFromServer(for url: String) -> [Product] {
         var productList:[Product] = []
-
         let product1 = Product(name: "Apple macbook", id: 1, description: "fajkjkfsld afsdlfdsaksdjfa dsfljafsdjdjk")
         let product2 = Product(name: "windows", id: 2, description: "fajkjkfsld afsdlfdsaksdjfa dsfljafsdjdjk")
-        
         productList.append(product1)
         productList.append(product2)
-       
         return productList
+    }
+    
+    func parseData() {
+        //TODO: - parsing code goes here , once done needs to remove this.
     }
 }
